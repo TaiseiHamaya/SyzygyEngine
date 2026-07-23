deffcfef4c70af7f47446b840bca53e744b887d2
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2026-07-24 00:10:12 +0900

    AlphaBlend使用時のdesc設定が正しくない不具合を修正しました。

--- Engine/GraphicsAPI/DirectX/DxPipelineState/PSOBuilder/PSOBuilder.cpp
+++ Engine/GraphicsAPI/DirectX/DxPipelineState/PSOBuilder/PSOBuilder.cpp
@@ -1,4 +1,4 @@
-﻿#include "PSOBuilder.h"
+#include "PSOBuilder.h"
 
 #include "Engine/Application/Logger.h"
 #include "Engine/Assets/Shader/ShaderAsset.h"
@@ -157,8 +157,8 @@ void PSOBuilder::blendstate(PsoBlendMode blendMode, u32 renderTarget) {
 		desc.BlendOp = D3D12_BLEND_OP_ADD;
 		desc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
 		desc.SrcBlendAlpha = D3D12_BLEND_ONE;
-		desc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
-		desc.DestBlendAlpha = D3D12_BLEND_ZERO;
+		desc.BlendOpAlpha = D3D12_BLEND_OP_MAX;
+		desc.DestBlendAlpha = D3D12_BLEND_ONE;
 		break;
 	case PsoBlendMode::Add:
 		desc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
